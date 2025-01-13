f3757bc07d63bb78736f259c0fe9f7b194f56f10
danielace1 <danielsudharsan1805@gmail.com>
2025-01-13 19:53:52 +0530

    feat: added models, controllers

--- server/index.js
+++ server/index.js
@@ -6,16 +6,24 @@ import commentRouter from "./routes/comment.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  console.log(error.message);
+  res.json({
+    message: error.message || "Something went wrong",
+    status: error.status || 500,
+    stack: error.stack,
+  });
+  next();
+});
 
 app.listen(PORT, (req, res) => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 5000");
 });
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import {
+  getPosts,
+  getPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("post routes");
-});
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+router.post("/writeblog", createPost);
+router.delete("/:id", deletePost);
 
 export default router;
