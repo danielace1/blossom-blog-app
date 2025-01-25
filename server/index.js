a5210a21f4ce79d02356f280834cf01a2125f460
danielace1 <danielsudharsan1805@gmail.com>
2025-01-25 15:52:30 +0530

    docs: added readme, license and deployed URLs

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://blossom-blog-app.onrender.com"],
     credentials: true,
   })
 );
